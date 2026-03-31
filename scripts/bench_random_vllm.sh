515e892c4ef5c6308b7fb5bc385a70abd497b151
Lorenzo Pozzi <lpozzi@pcopernico.it>
2026-03-31 16:16:50 +0200

    fixed bench_random_vllm

--- scripts/bench_random_vllm.sh
+++ scripts/bench_random_vllm.sh
@@ -2,23 +2,23 @@
 # input and output token lengths, and saving one result file per configuration.
 
 MODEL="mistralai/Ministral-3-3B-Instruct-2512"
-NUM_PROMPTS=50
-REQUEST_RATE=5
-MAX_CONC=10
+NUM_PROMPTS=100
+REQUEST_RATE=10
+MAX_CONC=32
 PREFIX_LEN=100
 RESULTS_DIR="./results/vllm_random_results"
 
 mkdir -p $RESULTS_DIR
 
-INPUT_LENS=(7500 10000)
-OUTPUT_LENS=(50 100 150 200 250)
+INPUT_LENS=(2500)
+OUTPUT_LENS=(50 250 500)
 
 echo "Start benchmark sweep..."
 
 for in_len in "${INPUT_LENS[@]}"; do
     for out_len in "${OUTPUT_LENS[@]}"; do
 
-        FILE_NAME="${RESULTS_DIR}/res_in${in_len}_out${out_len}.json"
+        FILE_NAME="res_in${in_len}_out${out_len}.json"
 
         echo "-------------------------------------------------------"
         echo "Executing: Input $in_len | Output $out_len"
@@ -32,7 +32,8 @@ for in_len in "${INPUT_LENS[@]}"; do
             --request-rate $REQUEST_RATE \
             --max-concurrency $MAX_CONC \
             --random-prefix-len $PREFIX_LEN \
-            --result-dir $FILE_NAME \
+            --result-dir $RESULTS_DIR \
+            --result-filename $FILE_NAME \
             --save-result
 
     done
